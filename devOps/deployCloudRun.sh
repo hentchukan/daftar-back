33759804a45ebe46bc28190d4b51ab64f038a024
iori-yagami <ferchichi.farooq@gmail.com>
2025-04-13 22:14:27 +0200

    fix(daftar-back): all time active back

--- devOps/deployCloudRun.sh
+++ devOps/deployCloudRun.sh
@@ -2,7 +2,11 @@ gcloud run deploy daftar-back \
   --image gcr.io/daftar-360022/daftar-back \
   --platform managed \
   --region us-central1 \
-  --allow-unauthenticated
+  --allow-unauthenticated \
+  --min-instances=1 \
+  --memory=512Mi \
+  --cpu=1 \
+  --port=8080
 
 gcloud run deploy daftar-front \
   --image gcr.io/daftar-360022/daftar-front \
